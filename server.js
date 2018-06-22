773a996c763dace9714ec6fe378164558b1599be
karava <kishan.arava@gmail.com>
2018-06-23 09:30:09 +1000

    new server file

--- server.js
+++ server.js
@@ -6,8 +6,39 @@
 // }).listen(8080, 'localhost');
 // console.log('Server running at http://localhost:8080/');
 
+
+
+
+// var express = require('express');
+// var app = express();
+
+// app.use(express.static(__dirname + "/build/"));
+// app.listen(5000);
+
+
+
+
+// var express = require('express');
+// var app = express();
+
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, '/build/'), function(err) {
+//     if (err) {
+//       res.status(500).send(err)
+//     }
+//   })
+// })
+// app.listen(5000);
+
+
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/build/"));
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + "/build/", function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 app.listen(5000);
