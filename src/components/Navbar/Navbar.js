52e2bde1939c5473d49803c6623a88290a24a97e
karava <kishan.arava@gmail.com>
2018-06-27 07:24:32 +1000

    replaced ahrefs with Link elements in Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 import './Navbar.css';
 
 class Navbar extends Component {
   render() {
     return (
-       <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="/"> 
-		<img src="img/logo.png" height="60px" width="150px" alt=""/></a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-		   <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="/">Home</a>
-            </li> 
-			  <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="/what-we-do">What we do </a>
-            </li>
-			 <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="/our-work">Our work </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="/contact">Contact</a>
-            </li>
+      <a class="navbar-brand js-scroll-trigger" href="/"> 
+		  <img src="img/logo.png" height="60px" width="150px" alt=""/></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+		      
+          <li class="nav-item">
+            <Link className="nav-link js-scroll-trigger" to="/">Home</Link>
+          </li> 
+			    <li class="nav-item">
+            <Link className="nav-link js-scroll-trigger" to="/what-we-do">What we do </Link>
+          </li>
+			    <li class="nav-item">
+            <Link className="nav-link js-scroll-trigger" to="/our-work">Our work </Link>
+          </li>
+          <li class="nav-item">
+            <Link className="nav-link js-scroll-trigger" to="/contact">Contact</Link>
+          </li>
+          
           </ul>
         </div>
       </div>
