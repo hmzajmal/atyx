e090d3dbf87985a7fd125be5a439a847abe10c72
karava <kishan.arava@gmail.com>
2018-07-04 11:29:42 +1000

    Update HomeSection2.js

--- src/components/HomePage/HomeSection2/HomeSection2.js
+++ src/components/HomePage/HomeSection2/HomeSection2.js
@@ -13,14 +13,14 @@ class HomeSection2 extends Component {
             <div class="service-box mt-5 mx-auto">
               <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
               <h4 class="mb-3">Software and Systems Development</h4>
-              <p class="text-muted mb-0">Custom software solutions to boost productivity, improve analytics and improve decision making.</p>
+              <p class="text-muted mb-0">Custom software solutions to boost productivity, harness insights and improve decision making.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
               <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
               <h4 class="mb-3">Technology Consulting</h4>
-              <p class="text-muted mb-0">Helping you choose, implement and deploy the best technologies for any business process.</p>
+              <p class="text-muted mb-0">Helping you choose, implement and deploy the best-fit solutions for any business process.</p>
             </div>
           </div>
           <div class="col-lg-4 col-md-6 text-center">
