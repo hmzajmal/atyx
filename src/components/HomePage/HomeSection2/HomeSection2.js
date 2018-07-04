5ca4a0836672d9539169653e699d2bf60a259df7
karava <kishan.arava@gmail.com>
2018-07-04 11:02:13 +1000

    Update HomeSection2.js

--- src/components/HomePage/HomeSection2/HomeSection2.js
+++ src/components/HomePage/HomeSection2/HomeSection2.js
@@ -26,7 +26,7 @@ class HomeSection2 extends Component {
           <div class="col-lg-4 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
               <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i>
-              <h4 class="mb-3">Vanguard Services</h4>
+              <h4 class="mb-3">Vanguard Solutions</h4>
               <p class="text-muted mb-0">Development services for cutting edge technologies like artificial intelligence (AI), machine learning and blockchain solutions.</p>
             </div>
           </div>
