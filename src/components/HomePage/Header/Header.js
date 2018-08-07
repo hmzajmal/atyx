f1e28cb5f23c4bfcd123320b4d684620c49fef47
Hamza Jamal <hmzajmal911@gmail.com>
2018-08-07 12:44:55 +0500

    solved

--- src/components/HomePage/Header/Header.js
+++ src/components/HomePage/Header/Header.js
@@ -5,30 +5,30 @@ import './Header.css';
 class Header extends Component {
   render(props) {
 
-	   
+
     return (
-        /* <div className="callout primary" id="Header">
-        <div className="row column">
-          <h1>{this.props.name}</h1>
-        </div>
+      /* <div className="callout primary" id="Header">
+      <div className="row column">
+        <h1>{this.props.name}</h1>
       </div>
-	  */
-	  <header class="masthead text-center text-white d-flex">
-      <div class="container my-auto">
-        <div class="row">
-          <div class="col-lg-10 mx-auto">
-            <h1 class="text-uppercase">
-              <strong>ATYX</strong>
-            </h1>
-            <hr/>
-          </div>
-          <div class="col-lg-8 mx-auto">
-            <p class="text-faded mb-5">Smarter Systems and Software Development</p>
-            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+    </div>
+  */
+      <header class="masthead text-center text-white d-flex">
+        <div class="container my-auto">
+          <div class="row">
+            <div class="col-lg-10 mx-auto">
+              <h1 class="text-uppercase">
+                <strong>ATYX</strong>
+              </h1>
+              <hr />
+            </div>
+            <div class="col-lg-8 mx-auto">
+              <p class="text-faded mb-5">Smarter Systems and Software Development</p>
+              <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find out more</a>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
     );
   }
 }
